4fc2ccb0a42289e17fed3672034d9ee9b77dd953
unknown <filip.dutina@gmail.com>
2019-11-04 23:27:32 +0100

    Full code rework

--- venv/main.py
+++ venv/main.py
@@ -5,144 +5,122 @@ import string
 import time
 import paho.mqtt.client as mqtt
 import networkx as nx
+from collections import defaultdict
 
-class VertexColor(Enum):
-    BLACK = 0
-    GRAY = 127
-    WHITE = 255
+class Graph:
+    nodes = list()
+    edges = list()
 
-class Publisher:
-    message = ''
-    topic = ''
-    def __init__(self, message = None, topic = None):
-        self.message = message
-        self.topic = topic
+    # Constructor
+    def __init__(self):
+        self.nodes = list()
+        self.edges = list()
 
-class Subscriber:
-    message = ''
-    topic = ''
-    def __init__(self, message = None, topic = None):
-        self.message = message
-        self.topic = topic
+    # Function to print a BFS of graph
+    def BFS(self, s):
+        print("\nFollowing is Breadth First Search:")
 
-class Vertex:
-    """
-    Graph vertex: A graph vertex (node)
-    """
-    def __init__(self, name = None, color = None, parent = None):
-        """
-        Vertex constructor
-        """
-        self.name = name
-        self.color = color
-        self.parent = parent
-        self.edgeList = list()
-        self.subscribers = list()
+        # Mark all the vertices as not visited
+        visited = [False] * (len(self.nodes))
 
-class Edge:
-    def __init__(self, source = None, destination = None, weight = None):
-        self.source = source
-        self.destination = destination
-        self.weight = weight
+        # Create a queue for BFS
+        queue = list()
 
-def Breadth_First_Search(graph, current_vertex):
-    for vertex in graph:
-        vertex.color = VertexColor.WHITE
-        vertex.parent = None
+        # Mark the source node as visited and enqueue it
+        queue.append(s)
+        visited[s] = True
 
-    current_vertex.color = VertexColor.GRAY
-    current_vertex.parent = None
-    Queue = list()
-    Queue.append(current_vertex)
-    while len(Queue) is not 0:
-        vertex_parent = Queue.pop(0)
-        for v in graph:
-            if (v.color == VertexColor.WHITE):
-                v.color = VertexColor.GRAY
-                v.parent = vertex_parent
-                Queue.append(v)
-                print(v.name)
+        while queue:
+            # Dequeue a vertex from queue and print it
+            s = queue.pop(0)
+            print(s, end=" ")
 
-        current_vertex.color = VertexColor.BLACK
+            # Get all adjacent vertices of the
+            # dequeued vertex s. If a adjacent
+            # has not been visited, then mark it
+            # visited and enqueue it
+            for i in self.nodes:
+                if visited[i] == False:
+                    queue.append(i)
+                    visited[i] = True
+        print()
 
 def Random_String(stringLength = 5):
     """Generate a random string of fixed length """
     letters = string.ascii_lowercase
     return ''.join(random.choice(letters) for i in range(stringLength))
 
-def Print_Vertex(vertex):
-    for i in vertex:
-        print(i.name)
+def Print_Vertex(graph):
+    print("\n***************************Graph***************************\n")
+    for i in graph.nodes:
+        print(i)
     print()
-    for j in i.edgeList:
-        print(j.destination.name)
+    for value in graph.edges:
+        print(str(value[0]) + ' ' + str(value[1]))
 
+def Get_Predefined_Dag():
+    nodes = 3
+    adjacency = list()
+
+    adjacency.append((0, 1))
+    adjacency.append((1, 2))
+
+    G = Graph()
+    # Append nodes
+    for i in range(nodes):
+        G.nodes.append(i)
+    # Append adjacencies
+    for i in range(len(adjacency)):
+        G.edges.append(adjacency[i])
+
+    return G
 
 def Get_Random_Dag():
-    MIN_PER_RANK = 1    # Nodes/Rank: How 'fat' the DAG should be
+    # Nodes/Rank: How 'fat' the DAG should be
+    MIN_PER_RANK = 1
     MAX_PER_RANK = 2
-    MIN_RANKS = 6   # Ranks: How 'tall' the DAG should be
+    # Ranks: How 'tall' the DAG should be
+    MIN_RANKS = 6
     MAX_RANKS = 10
-    PERCENT = 0.3  # Chance of having an Edge
+    # Chance of having an Edge
+    PERCENT = 0.3
     nodes = 0
+    adjacency = list()
 
     ranks = random.randint(MIN_RANKS, MAX_RANKS)
 
-    vertex_list = []
     for i in range(ranks):
         # New nodes of 'higher' rank than all nodes generated till now
         new_nodes = random.randint(MIN_PER_RANK, MAX_PER_RANK)
-
         # Edges from old nodes ('nodes') to new ones ('new_nodes')
         for j in range(nodes):
             for k in range(new_nodes):
                 if random.random() < PERCENT:
-                    #sredi ovo!!!!!!!
-                    vertex_list.append(Vertex(edgeList.append = (j, k + nodes)))
+                    adjacency.append((j, k + nodes))
 
         nodes += new_nodes
-    print(ranks)
 
-    return vertex_list
+    # Compute transitive graph
+    G = Graph()
+    # Append nodes
+    for i in range(nodes):
+        G.nodes.append(i)
+    # Append adjacencies
+    for i in range(len(adjacency)):
+        G.edges.append(adjacency[i])
+
+    return G
 
 """
 Main function
 """
 if __name__ == "__main__":
-    vertex_list = list()
 
-    B1 = Vertex(name='B1')
-    B2 = Vertex(name='B2')
-    B3 = Vertex(name='B3')
+    random_graph = Get_Random_Dag()
+    Print_Vertex(random_graph)
 
-    #edges
-    B1.edgeList.append(Edge(B1, B2, 1))
-    B2.edgeList.append(Edge(B2, B3, 1))
+    predefined_graph = Get_Predefined_Dag()
+    Print_Vertex(predefined_graph)
 
-    #subscribers
-    B1.subscribers.append(Subscriber('', 'Topic1'))
-    B1.subscribers.append(Subscriber('', 'Topic2'))
+    random_graph.BFS(0)
 
-    B2.subscribers.append(Subscriber('', 'Topic1'))
-    B2.subscribers.append(Subscriber('', 'Topic2'))
-    B2.subscribers.append(Subscriber('', 'Topic3'))
-
-    B3.subscribers.append(Subscriber('', 'Topic1'))
-
-    vertex_list.append(B1)
-    vertex_list.append(B2)
-    vertex_list.append(B3)
-
-    #Generate_Random_Graph()
-    #Print_Vertex(vertex_list)
-    vertex_list = Get_Random_Dag()
-    #Print_Vertex(vertex_list)
-    """ print("\n\n****************************************first print***********************************************")
-    Print_Vertex(vertex_list)
-    print("\n\n****************************************second print***********************************************")
-    Breadth_First_Search(vertex_list, B1)
-    print("\n\n****************************************Generate_Random_Graph()***********************************************")
-    vertex_list = Generate_Random_Graph()
-    Print_Vertex(vertex_list)
-    print("\n------------------------------------------> BFS \n")
-    Breadth_First_Search(vertex_list, vertex_list[0])"""
